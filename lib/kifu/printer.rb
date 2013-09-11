8940b699f8311e41ceb19c81ab39b7276696526c
Sergey Kviatkovsky <crono.neko@gmail.com>
2013-09-12 01:14:13 +0300

    Print game info: print header

--- lib/kifu/printer.rb
+++ lib/kifu/printer.rb
@@ -1,13 +1,21 @@
 module Kifu
   class Printer
-    attr_reader :game
+    attr_reader :game, :output
 
     def initialize(game)
       @game = game
+      @output = ''
     end
 
     def print_info
+      output << print_header
+    end
 
+    private
+
+    def print_header
+      header = "#{game.white_name} vs #{game.black_name}"
+      "#{header}\n#{ '-' * header.length }"
     end
   end
 end
--- spec/printer_spec.rb
+++ spec/printer_spec.rb
@@ -8,9 +8,23 @@ describe Kifu::Printer do
     it "stores game" do
       expect(printer.game).to eq game
     end
+
+    it "creates new output" do
+      expect(printer.output).to eq ''
+    end
   end
 
   describe "#print_info" do
+    let(:output) { printer.output }
 
+    before do
+      game.black_name = 'Black'
+      game.white_name = 'White'
+      printer.print_info
+    end
+
+    it "prints header" do
+      expect(output).to include "White vs Black\n--------------"
+    end
   end
 end
