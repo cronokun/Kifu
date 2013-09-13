bc31242d9d45ca2b08dd1e5e10804eeb9f668ffe
Sergey Kviatkovsky <crono.neko@gmail.com>
2013-09-13 17:32:34 +0300

    More meaningfull test data

--- spec/printer_spec.rb
+++ spec/printer_spec.rb
@@ -18,14 +18,14 @@ describe Kifu::Printer do
     let(:output) { printer.output }
 
     before do
-      game.black_name = 'Black'
-      game.white_name = 'White'
+      game.black_name = 'Otake Hideo'
+      game.white_name = 'Takemiya Masaki'
       game.comment = 'This is a test game'
       printer.print_info
     end
 
     it "prints header" do
-      expect(output).to include "White vs Black\n--------------"
+      expect(output).to include "Takemiya Masaki vs Otake Hideo\n------------------------------"
     end
 
     it "prints game comment" do
