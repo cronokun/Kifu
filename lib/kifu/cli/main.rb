063f2d906a1011da9179c9c95640c1292a0c5e83
Sergey Kviatkovsky <crono.neko@gmail.com>
2013-09-21 23:35:04 +0300

    Extract info command using thor

--- lib/kifu/cli/main.rb
+++ lib/kifu/cli/main.rb
@@ -1,32 +1,16 @@
+require 'thor'
+
 module Kifu
   module CLI
-    module Main
+    class Main < Thor
 
-      def main
-        if ARGV.empty?
-          print_short_usage
-          exit
-        end
-
-        command = ARGV.shift
-        file = ARGV.shift
-
-        case command
-        when "info"
-          parsed_data = Kifu::SgfReader.new(file).parse
-          game        = Kifu::GameBuilder.new.build_from_sgf(parsed_data)
-          printer     = Kifu::Printer.new(game)
-          printer.print_info
-          $stdout.puts printer.output
-        else
-          print_short_usage
-        end
-      end
-
-      private
-
-      def print_short_usage
-        puts "Usage: kifu command [arguments...] [options...]"
+      desc 'info FILE', 'print basic game info'
+      def info(file)
+        parsed_data = Kifu::SgfReader.new(file).parse
+        game        = Kifu::GameBuilder.new.build_from_sgf(parsed_data)
+        printer     = Kifu::Printer.new(game)
+        printer.print_info
+        $stdout.puts printer.output
       end
 
     end
